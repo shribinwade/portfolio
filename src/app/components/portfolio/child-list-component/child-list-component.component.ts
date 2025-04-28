96d468336c9540f427c98ef40705bb60147b8c9e
shrihari <shribinwade.9103@gmail.com>
2025-04-29 00:16:03 +0530

    project horizontal scroll added

--- src/app/components/portfolio/child-list-component/child-list-component.component.ts
+++ src/app/components/portfolio/child-list-component/child-list-component.component.ts
@@ -1,30 +1,61 @@
+import { AfterViewInit, Component, ElementRef, HostBinding, Input, ViewChild } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-
-interface List{
-  id:number,
-  img:string,
-  title:string,
-  dec:string,
-  link:string
+interface List {
+  id: number,
+  img: string,
+  title: string,
+  dec: string,
+  link: string
 }
-
-
-
-import { Component, Input } from '@angular/core';
-
 
 @Component({
   selector: 'app-child-list-component',
-  imports: [],
+  standalone: true,
   templateUrl: './child-list-component.component.html',
   styleUrl: './child-list-component.component.css'
 })
-export class ChildListComponentComponent {
-
-  @Input('list') list!:List;
-
-  constructor(){
-
+export class ChildListComponentComponent implements AfterViewInit {
+  @Input('list') list!: List;
+  @Input() nthIndex: number = 0;
+  @Input() projectRef!: string;
+  
+  @HostBinding('class') get hostClasses(): string {
+    switch (this.nthIndex) {
+      case 0: return 'first';
+      case 1: return 'second';
+      case 2: return 'third';
+      case 3: return 'fourth';
+      case 4: return 'fifth';
+      default: return '';
+    }
   }
 
+  @ViewChild('project') project!: ElementRef<HTMLDivElement>;
+
+  constructor() {
+    gsap.registerPlugin(ScrollTrigger);
+  }
+
+  ngAfterViewInit(): void {
+    const project = this.project.nativeElement;
+    
+    gsap.from(project, {
+      scrollTrigger: {
+        trigger: project,
+        start: 'top 81.5%',
+        end: 'bottom 81.5%',
+        toggleActions: 'restart none none reverse',
+        markers: true,
+        scrub: 1
+      },
+      opacity: 0,
+      stagger:1,
+      x: -100,
+      y: 50,
+      duration: 1,
+      ease: 'power2.out'
+    });
+  }
 }
--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { ChildListComponentComponent } from "./child-list-component/child-list-component.component";
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit, QueryList, ViewChildren } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ChildListComponentComponent } from './child-list-component/child-list-component.component';
 
 @Component({
   selector: 'app-portfolio',
@@ -8,38 +10,103 @@ import { ChildListComponentComponent } from "./child-list-component/child-list-c
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
+export class PortfolioComponent implements OnInit, AfterViewInit {
+  @ViewChild('panelContainer') panelContainer!: ElementRef;
+  @ViewChild('pList') pList!: ElementRef;
+  @ViewChildren('project') projects!: QueryList<ElementRef>;
 
-export class PortfolioComponent {
 
- protected items = [
+  items = [
     { 
       id: 1,
       img: 'portfolioImg.png',
-      title: "test",
-      dec:"test",
-      link:'/'
+      title: "Project 1",
+      dec: "Description 1",
+      link: '/'
     },
     { 
       id: 2,
-      img: 'api.svg',
-      title: "test",
-      dec:"test",
-      link:'/'
+      img: 'portfolioImg.png',
+      title: "Project 2",
+      dec: "Description 2",
+      link: '/'
     },
     { 
       id: 3,
-      img: 'api.svg',
-      title: "test",
-      dec:"test",
-      link:'/'
+      img: 'portfolioImg.png',
+      title: "Project 3",
+      dec: "Description 3",
+      link: '/'
     },
     { 
       id: 4,
-      img: 'api.svg',
-      title: "test",
-      dec:"test",
-      link:'/'
+      img: 'portfolioImg.png',
+      title: "Project 4",
+      dec: "Description 4",
+      link: '/'
     },
-  ]
+    { 
+      id: 5,
+      img: 'portfolioImg.png',
+      title: "Project 5",
+      dec: "Description 5",
+      link: '/'
+    }
+  ];
 
+  constructor() {
+    gsap.registerPlugin(ScrollTrigger);
+  }
+
+  ngAfterViewInit(): void {
+    this.initHorizontalScroll();
+    // this.initAnimation();
+    // this.projects.changes.subscribe(() => {
+    //   this.initAnimation();
+    // });
+  }
+
+  initAnimation(){
+    // const boxes = this.projects.map(el => el.nativeElement);
+    
+    // gsap.from(boxes, {
+    //   scrollTrigger: {
+    //     trigger: boxes,
+    //     start: 'top 81.5%',
+    //     end: 'bottom 81.5%',
+    //     toggleActions: 'restart none none reverse',
+    //     markers: false,
+    //   },
+    //   opacity: 0,
+    //   x: -100,
+    //   y: 50,
+    //   duration: 1,
+    //   stagger: 0.3,
+    //   ease: 'power2.out'
+    // });
+
+  
+
+
+  }
+
+  initHorizontalScroll(): void {
+    const container = this.panelContainer.nativeElement;
+    const list = this.pList.nativeElement;
+
+    gsap.to(list, {
+      x: () => -(list.scrollWidth - window.innerWidth ),
+      ease: 'none',
+      scrollTrigger: {
+        trigger: container,
+        pin: true,
+        scrub: 1,
+        start: 'top top',
+        end: () => `+=${list.scrollWidth - window.innerWidth}`,
+        invalidateOnRefresh: true
+      }
+    });
+  }
+
+  ngOnInit(): void {}
 }
